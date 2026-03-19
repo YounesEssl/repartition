925c1997c697c6e42cf2f29dd8f4674e2705fcb0
YounesEssl <younes.esslimani@efrei.net>
2026-03-19 21:12:24 +0100

    tedsa

--- lib/redis.ts
+++ lib/redis.ts
@@ -1,9 +1,6 @@
 import { Redis } from "@upstash/redis";
 
-export const redis = new Redis({
-  url: process.env.KV_REST_API_URL!,
-  token: process.env.KV_REST_API_TOKEN!,
-});
+const redis = Redis.fromEnv();
 
 const STATE_KEY = "room-assignments";
 
